9b1cafdc9007a8ce1cabbfbe0091881f4bab0945
Dragon <33269567+gotodragon@users.noreply.github.com>
2020-03-28 23:00:27 +0800

    Create original_x64openwrt.sh

---
+++ original_x64openwrt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: Allcloud
+#=================================================
+#1. Modify default IP
+sed -i 's/192.168.1.1/10.0.0.111/g' openwrt/package/base-files/files/bin/config_generate
